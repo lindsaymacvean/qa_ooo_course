ac3ad1bb20719488c1612f3c4ae93e684526a42f
lindsaymacvea <lindsaymacvean@gmail.com>
2017-10-23 22:35:12 +0000

    Initial Commit

---
+++ The Basics Part 1/Starters/DeclaringVariables/Program.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeclaringVariables {
+  class Program {
+
+    static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+      
+      // A number of tasks follow
+      // Read them carefully and perform them sequentially
+      // Do not worry about any 'green' underlines.
+      // But correct any 'red' ones!
+      // We recommend you delete comments as you go along.
+      // (starting now with these 6 statements !! )
+
+      // Task 1
+      // Note how the 'SolutionName'/'ProjectName'/'NameSpace'
+      // 'ClassName ('Program')'/'MethodName'(Main) all start with an uppercase character
+      // Note though how the parameter of this Main method ('args') 
+      // which is local to this method starts with a lowercase character
+      // Ensure you stick to 'camelCasing' for all local variables you declare
+      //                      X    X
+
+      // Task 1
+      // Declare a variable of type int call it 'age'. Do not intiialise it.
+      
+
+      // Task 2 
+      // Declare but again do not initialise, in 1 statement only
+      // 2 variables of type int to store 'my age' and 'your age'
+      
+
+      // Task 3
+      // Declare in one statement only, 2 ints called 'hisAge' and 'herAge'
+      // Initialise 'hisAge' to a suitable value AT THE POINT OF DECLARATION
+      
+
+      // Task 4
+      // Try and display ('cw' Tab-Tab) 'herAge'. 
+      // It will not compile, read the error message, it is not initialised
+      
+
+      // Task 5
+      // Comment out the line just coded above, and now initialise 'herAge' 
+      // here and try displaying it again. Check the project 'builds' Ctrl-Shift-B
+      
+
+      // Task 6
+      // After each description below declare variables of the right type 
+      // with a sensible name and then initialise them with sensible values 
+      // based on the description
+
+      // The average number of children that 100 random families might have
+      
+      // The probability of you passing a serious C# exam today
+      
+      // The amount of money you would have if you won exactly £1Billion  
+      // on the lottery for 5 successive weeks
+      
+      // Your last name
+      
+      // The first character of your first name
+      //           ---- 
+      
+      // The price of a litre of milk
+
+      // Whether you had a good breakfast or not today
+      
+      // Feel free to display any of these values on the Console
+      // but typing as little as possible in doing so!! (7 keystokes per line?)
+      
+      
+      // Task 7
+      // Ensure you can find the icons on the toolbar that enable you to
+      // 'comment out' & 'uncomment' a single line. Practice NOW.
+      
+
+      // Task 8
+      // Write a 'block' multiline comment /* etc
+      // that comments out the whole of the content of this 'Main' method  
+      
+      
+    }
+  }
+}
