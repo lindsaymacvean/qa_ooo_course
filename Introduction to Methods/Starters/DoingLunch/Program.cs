2aebc57b370d4cc5cd1154576271878ecbb68cc2
lindsay macvean <lindsaymacvean@gmail.com>
2017-11-19 23:02:12 +0000

    Finished DoingLunch

--- Introduction to Methods/Starters/DoingLunch/Program.cs
+++ Introduction to Methods/Starters/DoingLunch/Program.cs
@@ -8,19 +8,35 @@ namespace DoingLunch {
     static void Main(string[] args) {
 
       Console.WriteLine("What main dish would you like?");
+      string mainCourse = Console.ReadLine();
 
-
-
-
-
-      //Console.WriteLine("How many roast potatoes?");
+      int roastPotatoes = GetInteger("How many roast potatoes?");
       
-      //Console.WriteLine("How many brussel sprouts?");
+      int brusselSprouts = GetInteger("How many brussel sprouts?");
 
-     
       // Put the display here!
+      Console.WriteLine("Hello your lunch is {0} with {1} roast potatoes and {2} brussel sprouts.", 
+      mainCourse, roastPotatoes, brusselSprouts);
 
+      int remainingRoastPotatoes = RemainingRoastPotato(roastPotatoes, brusselSprouts);
+            Console.WriteLine("After adding an exra potato, cutting them into quarters and eating one brussel" +
+                " sprout with every quarter; there will be {0} quarters of roast potatoes left.", 
+                remainingRoastPotatoes);
+      Console.ReadLine();
     }
-   
+
+    static int GetInteger(string message) {
+       Console.WriteLine(message);
+       return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static int RemainingRoastPotato(int roastPotatoes, int brusselSprouts) {
+       roastPotatoes = roastPotatoes + 1;
+       int roastPotatoQuarters = roastPotatoes * 4;
+       Console.WriteLine(roastPotatoQuarters);
+       roastPotatoQuarters = roastPotatoQuarters % brusselSprouts;
+       // If its negative, display 0 as minimum
+       return Math.Max(0, roastPotatoQuarters);
+     }
   }
 }
