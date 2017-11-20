972d6a672afa703060c0b998d349786f4c424309
lindsay macvean <lindsaymacvean@gmail.com>
2017-11-20 23:31:22 +0000

    finished the when have time part of the EveningBehaviour exercise

--- Getting started with Conditionals/Starters/EveningBehaviour/Program.cs
+++ Getting started with Conditionals/Starters/EveningBehaviour/Program.cs
@@ -8,40 +8,50 @@ namespace EveningBehaviour {
     
     private static bool summerTime = true;
     private static bool sunnyEvening = true;
+    private static bool isWeekend = false;
 
     static void Main(string[] args) {
-
-        if (summerTime)
-            {
+        
+        if (!IsWeekend()) {
+            if (IsSummerTime()) {
                 TakeShower();
                 if (sunnyEvening)
                     Eat("outside");
                 else
                     Eat("inside");
-                DoGardening();
+                DoActivity("Gardening");
             }
 
-        if (!summerTime)
-            {
+            if (!IsSummerTime()) {
                 Eat("inside");
                 if (sunnyEvening)
-                    ReadPaperinSummerHouse();
-                DoPaperWork();
+                    Read("Paper", "summerhouse");
+                DoActivity("Paperwork");
                 TakeShower();
             }
-        ReadBookInBed();
-        Console.ReadLine();
-        Environment.Exit(0);
 
-            /* His behaviour varies primarily by whether it is ‘summertime’ or not but he always ends his day by ‘reading in bed’.
-    If it is ‘summertime’ he ‘takes a shower’ the moment he arrives home.
-    If it is also ‘a sunny evening’ he ‘eats outside’ otherwise he ‘eats inside’.
-    After eating he then ‘does some gardening’.
-    When it isn’t the summertime the evening starts by ‘eating inside’.
-    Then if it’s a ‘sunny evening’ he ‘reads the paper in the summerhouse’.
-    Then he always ‘does a little paperwork’ before taking a late ‘shower’. */
+            Read("book", "bed");
         }
 
+        if (IsWeekend()) {
+            string[] activity = {"boating", "cleaning", "polishing"};
+            Random rnd = new Random();
+            DoActivity(activity[rnd.Next(0, activity.Length)]);
+            DoActivity(activity[rnd.Next(0, activity.Length)]);
+            DoActivity(activity[rnd.Next(0, activity.Length)]);
+        }
+
+        TeethClean();
+        Console.ReadLine();
+
+        /* His behaviour varies primarily by whether it is ‘summertime’ or not but he always ends his day by ‘reading in bed’.
+        If it is ‘summertime’ he ‘takes a shower’ the moment he arrives home.
+        If it is also ‘a sunny evening’ he ‘eats outside’ otherwise he ‘eats inside’.
+        After eating he then ‘does some gardening’.
+        When it isn’t the summertime the evening starts by ‘eating inside’.
+        Then if it’s a ‘sunny evening’ he ‘reads the paper in the summerhouse’.
+        Then he always ‘does a little paperwork’ before taking a late ‘shower’. */
+    }
 
     public static bool IsSummerTime() {
       return summerTime;
@@ -49,25 +59,25 @@ namespace EveningBehaviour {
     public static bool IsSunnyEvening() {
       return sunnyEvening;
     }
+    public static bool IsWeekend() {
+      return isWeekend;
+    }
     
     public static void Eat(string where) {
       Console.WriteLine("Eating {0}", where);
-
     }
-    public static void DoGardening() {
-      Console.WriteLine("doing gardening");
-    }
-    public static void DoPaperWork() {
-      Console.WriteLine("doing paperWork");
+    public static void DoActivity(string activity) {
+      Console.WriteLine("doing {0}", activity);
     }
     public static void TakeShower() {
       Console.WriteLine("taking  shower");
     }
-    public static void ReadPaperinSummerHouse() {
-      Console.WriteLine("Reading the paper in the summerhouse");
+    public static void Read(string what, string where) {
+      Console.WriteLine("Reading {0} in the {1}", what, where);
     }
-    public static void ReadBookInBed() {
-      Console.WriteLine("Reading in bed");
+
+    public static void TeethClean() {
+      Console.WriteLine("Cleaning teeth");
     }
   }
 }
