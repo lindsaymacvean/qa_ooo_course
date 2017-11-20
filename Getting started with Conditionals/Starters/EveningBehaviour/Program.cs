4caed77029bc0d63a2fd6fd298aad51cc631f2d9
lindsay macvean <lindsaymacvean@gmail.com>
2017-11-20 23:08:04 +0000

    finished EveningBehaviour task

--- Getting started with Conditionals/Starters/EveningBehaviour/Program.cs
+++ Getting started with Conditionals/Starters/EveningBehaviour/Program.cs
@@ -6,16 +6,43 @@ using System.Text;
 namespace EveningBehaviour {
   class Program {
     
-    private static bool summerTime = false;
-    private static bool sunnyEvening = false;
+    private static bool summerTime = true;
+    private static bool sunnyEvening = true;
 
     static void Main(string[] args) {
-      
 
-    }
+        if (summerTime)
+            {
+                TakeShower();
+                if (sunnyEvening)
+                    Eat("outside");
+                else
+                    Eat("inside");
+                DoGardening();
+            }
+
+        if (!summerTime)
+            {
+                Eat("inside");
+                if (sunnyEvening)
+                    ReadPaperinSummerHouse();
+                DoPaperWork();
+                TakeShower();
+            }
+        ReadBookInBed();
+        Console.ReadLine();
+        Environment.Exit(0);
+
+            /* His behaviour varies primarily by whether it is ‘summertime’ or not but he always ends his day by ‘reading in bed’.
+    If it is ‘summertime’ he ‘takes a shower’ the moment he arrives home.
+    If it is also ‘a sunny evening’ he ‘eats outside’ otherwise he ‘eats inside’.
+    After eating he then ‘does some gardening’.
+    When it isn’t the summertime the evening starts by ‘eating inside’.
+    Then if it’s a ‘sunny evening’ he ‘reads the paper in the summerhouse’.
+    Then he always ‘does a little paperwork’ before taking a late ‘shower’. */
+        }
 
 
-    #region "Methods to be called"
     public static bool IsSummerTime() {
       return summerTime;
     }
@@ -42,6 +69,5 @@ namespace EveningBehaviour {
     public static void ReadBookInBed() {
       Console.WriteLine("Reading in bed");
     }
-    #endregion
   }
 }
