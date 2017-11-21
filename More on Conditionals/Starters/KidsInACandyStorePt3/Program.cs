d99e312b103bca366d043fdc2d581af45aa0e86f
lindsay macvean <lindsaymacvean@gmail.com>
2017-11-21 12:02:55 +0000

    Finished Kids in a candy store pt3. I didnt understand why I needed returns when an easier way of simplifying the code was to just remove braces. I also converted from pennies to pounds using decimal type.

--- More on Conditionals/Starters/KidsInACandyStorePt3/Program.cs
+++ More on Conditionals/Starters/KidsInACandyStorePt3/Program.cs
@@ -3,51 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace KidsInCandyStorePt3 {
-  class Program {
-    static void Main(string[] args) {
-
-      int price = GetInteger("Price of a bag please?");
-      if (price > 0)
-      {
-        int money = GetInteger("How much money do you have?");
-
-        if (money > 0)
+namespace KidsInCandyStorePt3
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
-          int numBags = (money / price);
 
-          Console.WriteLine("If price is {0}p and you have {1}p you will be able to purchase {2} bags and will have {3}p left over",
-                              price, money, numBags, money % price);
+            decimal price = GetDecimal("Price of a bag please?");
+            if (price > 0)
+            {
+                decimal money = GetDecimal("How much money do you have?");
+                if (money > 0)
+                {
+                    decimal numBags = (money / price);
+                    Console.WriteLine("If price is £{0} and you have £{1} you will be able to purchase " +
+                        "{2} {4} and will have £{3} left over",
+                        price, money, numBags, money % price, numBags > 1 ? "bags":"bag");
+                }
+                else if (money == 0) // no pocket money
+                    Console.WriteLine("Come back with your pocket money!");
+                else
+                    Console.WriteLine("Money must be a non-negative numeric value");
+            }
+            else if (price == 0) // free
+                Console.WriteLine("They are free take loads!!");
+            else // price is less than 0
+                Console.WriteLine("Price must be a non-negative numeric value");
+            Console.ReadLine();
         }
-        else if (money == 0) // no pocket money
-        {
-          Console.WriteLine("Come back with your pocket money!");
-        }
-        else
-        {
-          Console.WriteLine("Money must be a non-negative numeric value");
-        }
-      }
-      else if (price == 0) // free
-      {
-        Console.WriteLine("They are free take loads!!");
-      }
-      else // price is less than 0
-      {
-        Console.WriteLine("Price must be a non-negative numeric value");
-      }
 
+        static decimal GetDecimal(string message)
+        {
+            Console.WriteLine(message);
+            string sVal = Console.ReadLine();
+            decimal val;
+            if (!decimal.TryParse(sVal, out val)) // ie sVal not numeric
+            {
+                val = -1;
+            }
+            return val;
+        }
     }
-
-    static int GetInteger(string message) {
-      Console.WriteLine(message);
-      string sVal = Console.ReadLine();
-      int val;
-      if (!int.TryParse(sVal, out val)) // ie sVal not numeric
-      {
-        val = -1;
-      }
-      return val;
-    }
-  }
 }
