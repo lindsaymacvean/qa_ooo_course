184447a4f56829a42b51206b8cd23525e441d27c
lindsay macvean <lindsaymacvean@gmail.com>
2017-11-21 10:07:57 +0000

    Finished FlowControl exercise

--- Loops/Starters/Number/Number.cs
+++ Loops/Starters/Number/Number.cs
@@ -3,10 +3,25 @@ using System;
 namespace FlowControl {
     public class Number {
         public static void Main() {
-            
+            long result = 1, maxValue = 999999999L;
+            for (int i=1; i < 64; i ++)
+            {
+                result = result * 2;
+                if (result > maxValue)
+                {
+                    break;
+                }
+                PrintFormatted(result, i);
+            }
 
+            Console.ReadLine();
 
+        }
 
+        private static void PrintFormatted(long result, int i)
+        {
+            Console.Write(i.ToString().PadLeft(2) + "\t");
+            Console.WriteLine(result.ToString().PadLeft(10));
         }
     }
 }
