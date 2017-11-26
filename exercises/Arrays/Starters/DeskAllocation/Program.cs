070b9c768c2f126767301527b1094e6cd666eb2b
lindsay macvean <lindsaymacvean@gmail.com>
2017-11-26 12:30:59 +0000

    Finished the Desk allocation task but using anonymous functions instead.

--- exercises/Arrays/Starters/DeskAllocation/Program.cs
+++ exercises/Arrays/Starters/DeskAllocation/Program.cs
@@ -3,50 +3,109 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace DeskAllocation {
-  class Program {
-    public static string[,] deskNames;
-    
-    public static string[] names = 
-     {"Amy","Ben","Carly","Danesha","Evy","Faroukh",
-      "Greg","Heather","Ivy","Jaz","Keith","Monica",
-      "Nick"
-     }; // 13 names!
+namespace DeskAllocation
+{
+    class Program
+    {
+        public static string[,] deskNames;
 
-    public static void Main(string[] args) {
-      // initialise and size the array at 3 rows * 4 columns
-      
+        public static string[] names =
+        {"Amy","Ben","Carly","Danesha","Evy","Faroukh",
+            "Greg","Heather","Ivy","Jaz","Keith","Monica",
+            "Nick"
+        }; // 13 names!
 
-      AllocateAllDesks();
-
-      DisplayDeskMap();
-   
-      
-      SearchDesks("Jaz");
-      SearchDesks("Nick");
+        public static void Main(string[] args)
+        {
+            // initialise and size the array at 3 rows * 4 columns
+            deskNames = new string[3,4];
+            AllocateAllDesks();
+            DisplayDeskMap();
 
 
+            SearchDesks("Jaz");
+            SearchDesks("Nick");
+
+            Console.ReadLine();
+        }
+
+        public static void AllocateAllDesks()
+        {
+            foreach (string name in names)
+            {
+                AllocateDesk(name);
+            }
+        }
+
+        // called from AllocateAllDesks()
+        public static void AllocateDesk(string name)
+        {
+            Func<int, int, int> callback = delegate(int row, int col)
+            {
+                if (deskNames[row, col] == null)
+                {
+                    deskNames[row, col] = name;
+                    Console.WriteLine(name + " was assigned to desk " + row + ", " + col);
+                    return -1;
+                }
+                //Console.WriteLine("The desk was already assigned to " + deskNames[row, col]);
+                return 0;
+            };
+            LoopDesks(callback);
+        }
+
+        public static void DisplayDeskMap()
+        {
+            string rowNames = "|";
+            Func<int, int, int> callback = delegate(int row, int col)
+            {
+                rowNames += deskNames[row, col].PadLeft(8) + "\t|";
+                if (col == deskNames.GetUpperBound(1))
+                {
+                    Console.WriteLine(rowNames);
+                    rowNames = "|";
+                }
+                return 0;
+            };
+            LoopDesks(callback);
+        }
+
+        public static void SearchDesks(string name)
+        {
+            Func<int, int, int> callback = delegate (int row, int col)
+            {
+                if (deskNames[row, col] == name)
+                {
+                    Console.WriteLine("{0} is located at desk {1}, {2}", name, row, col);
+                    return 1;
+                }
+                else if (col == deskNames.GetUpperBound(1) && row == deskNames.GetUpperBound(0))
+                {
+                    Console.WriteLine("Could not find {0} at any of the desks", name);
+                    return -1;
+                }
+                return 0;
+            };
+            LoopDesks(callback);
+        }
+
+        public static void ClearAllDesks()
+        {
+
+        }
+
+        // Decided to implement a method to implement an anonymous function within the loop
+        public static void LoopDesks(Func<int, int, int> callBack)
+        {
+            row:  for (int row = 0; row <= deskNames.GetUpperBound(0); row++)
+            {
+                col: for (int col = 0; col <= deskNames.GetUpperBound(1); col++)
+                {
+                    int result = callBack(row, col);
+                    if (result != 0)
+                        return;
+                }
+            }
+        }
     }
-    public static void AllocateAllDesks() {
-
-    }
-
-    // called from AllocateAllDesks()
-    public static void AllocateDesk(string name) {
-      
-    }
-    
-    public static void DisplayDeskMap() {
-
-    }
-
-    public static void SearchDesks(string name) {
-   
-    }
- 
-    public static void ClearAllDesks() {
-
-    }
-
-  }
 }
--- exercises/Arrays/Starters/Numbers/Numbers.cs
+++ exercises/Arrays/Starters/Numbers/Numbers.cs
@@ -1,28 +1,68 @@
 namespace Numbers
 {
-	using System;
-	public class Numbers
-	{
-	
-		public static void Main(String[] args)
-		{
+    using System;
+    public class Numbers
+    {
 
-			
-			// Create and initialise an array of 8 ints
+        public static void Main(String[] args)
+        {
 
-			// Go off and write the PrintNumbers method
-      // THEN you can Invoke it from here 
-	
 
-      // Ask the user to enter a number (and ensure that it is numeric)
-			// Invoke your NumberFinder method and check 
-			// the return value from it to see if the number was found 
-      // printing out an appropriate message
+            // Create and initialise an array of 8 ints
+            int[] intArray = new int[] { 1, 3, 5, 7, 2, 4, 6, 8 };
 
-	
-		}  // end of Main method
+            // Go off and write the PrintNumbers method
+            // THEN you can Invoke it from here 
+            PrintNumbers(intArray);
 
-    // Write the PrintNumbers method
-	
-	}
+            int numToFind = GetInteger("What number are you looking for?");
+
+            // Ask the user to enter a number (and ensure that it is numeric)
+            // Invoke your NumberFinder method and check 
+            // the return value from it to see if the number was found 
+            // printing out an appropriate message
+            int indexPos = FindNumber(intArray, numToFind);
+            if (indexPos >= 0)
+                Console.WriteLine("Num {0} found at position {1}", numToFind, indexPos);
+            else
+                Console.WriteLine("Number {0} not found in list", numToFind);
+
+            Console.ReadLine();
+        }  // end of Main method
+
+        private static int GetInteger(string message)
+        {
+            Console.WriteLine(message);
+            string sVal = Console.ReadLine();
+            int val;
+            if (int.TryParse(sVal, out val))
+                return val;
+            else
+            {
+                Console.WriteLine("You need to enter a number\n");
+                return GetInteger(message);
+            }
+        }
+
+        private static int FindNumber(int[] intArray, int numToFind)
+        {
+            for (int i = 0; i <= intArray.GetUpperBound(0); i++)
+            {
+                if (intArray[i] == numToFind)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        // Write the PrintNumbers method
+        private static void PrintNumbers(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            
+        }
+    }
 }
